56c5d786722d3f32b5a20035eb415304f0053462
Scott McMillan <scott.andrew.mcmillan@gmail.com>
2019-01-31 14:12:04 -0600

    Update mvapich2_gdr building block for latest OSU distribution (#120)

--- hpccm/building_blocks/mvapich2_gdr.py
+++ hpccm/building_blocks/mvapich2_gdr.py
@@ -33,11 +33,12 @@ from hpccm.primitives.comment import comment
 from hpccm.primitives.copy import copy
 from hpccm.primitives.environment import environment
 from hpccm.primitives.shell import shell
+from hpccm.templates.ldconfig import ldconfig
 from hpccm.templates.rm import rm
 from hpccm.templates.wget import wget
 from hpccm.toolchain import toolchain
 
-class mvapich2_gdr(rm, wget):
+class mvapich2_gdr(ldconfig, rm, wget):
     """The `mvapich2_gdr` building blocks installs the
     [MVAPICH2-GDR](http://mvapich.cse.ohio-state.edu) component.
     Depending on the parameters, the package will be downloaded from
@@ -48,11 +49,14 @@ class mvapich2_gdr(rm, wget):
     recipe components are supported; please refer to the MVAPICH2-GDR
     documentation for more information.
 
-    The [GNU compiler](#gnu) building block should be installed prior
-    to this building block.
+    The [GNU compiler](#gnu) or [PGI compiler](#pgi) building blocks
+    should be installed prior to this building block.
 
     The [Mellanox OFED](#mlnx_ofed) building block should be installed
     prior to this building block.
+
+    The [gdrcopy](#gdrcopy) building block should be installed prior
+    to this building block.
 
     As a side effect, this building block modifies `PATH` and
     `LD_LIBRARY_PATH` to include the MVAPICH2-GDR build.
@@ -61,6 +65,10 @@ class mvapich2_gdr(rm, wget):
     compiler wrappers.  The toolchain can be passed to other
     operations that want to build using the MPI compiler wrappers.
 
+    Note: Using MVAPICH2-GDR on non-RHEL-based Linux distributions has
+    several issues, including compiler version mismatches and libnuma
+    incompatibilities.
+
     # Parameters
 
     cuda_version: The version of CUDA the MVAPICH2-GDR package was
@@ -68,36 +76,53 @@ class mvapich2_gdr(rm, wget):
     should match the version of CUDA provided by the base image.  This
     value is ignored if `package` is set.  The default value is `9.0`.
 
+    gnu: Boolean flag to specify whether a GNU build should be used.
+    The default value is True.
+
+    ldconfig: Boolean flag to specify whether the MVAPICH2-GDR library
+    directory should be added dynamic linker cache.  If False, then
+    `LD_LIBRARY_PATH` is modified to include the MVAPICH2-GDR library
+    directory. The default value is False.
+
     mlnx_ofed_version: The version of Mellanox OFED the
     MVAPICH2-GDR package was built against.  The version string format
     is X.Y.  The version should match the version of Mellanox OFED
     installed by the `mlnx_ofed` building block.  This value is
-    ignored if `package` is set.  The default value is `3.4`.
+    ignored if `package` is set.  The default value is `4.2`.
 
-    ospackages: List of OS packages to install prior to
-    installation.  For Ubuntu, the default values are `openssh-client`
-    and `wget`.  For RHEL-based Linux distributions, the default
-    values are `openssh-clients`, `perl` and `wget`.
+    ospackages: List of OS packages to install prior to installation.
+    For Ubuntu, the default values are `cpio`, `libnuma1`,
+    `openssh-client`, `rpm2cpio` and `wget`, plus `libgfortran3` if a
+    GNU compiled package is selected.  For RHEL-based Linux
+    distributions, the default values are `libpciaccess`,
+    `numactl-libs`, `openssh-clients`, and `wget`, plus `libgfortran`
+    if a GNU compiled package is selected.
 
-    package: Path to the package file relative to the local build
-    context.  The default value is empty.  If this is defined, the
-    package in the local build context will be used rather than
-    downloading the package from the web.  The package should
+    package: Specify the package name to download.  The package should
     correspond to the other recipe components (e.g., compiler version,
-    CUDA version, Mellanox OFED version).
+    CUDA version, Mellanox OFED version).  If specified, this option
+    overrides all other building block options (e.g., compiler family,
+    compiler version, CUDA version, Mellanox OFED version,
+    MVAPICH2-GDR version).
+
+    pgi: Boolean flag to specify whether a PGI build should be used.
+    The default value is False.
 
     version: The version of MVAPICH2-GDR to download.  The value is
-    ignored if `package` is set.  The default value is `2.3a`.
+    ignored if `package` is set.  The default value is `2.3`.  Due to
+    differences in the packaging scheme, versions prior to 2.3 are not
+    supported.
 
     # Examples
 
     ```python
-    mvapich2_gdr(version='2.3a')
+    mvapich2_gdr(version='2.3')
     ```
 
     ```python
-    mvapich2_gdr(package='mvapich2-gdr-mcast.cuda9.0.mofed3.4.gnu4.8.5_2.3a-1.el7.centos_amd64.deb')
+    mvapich2_gdr(package='mvapich2-gdr-mcast.cuda10.0.mofed4.3.gnu4.8.5-2.3-1.el7.x86_64.rpm')
     ```
+
     """
 
     def __init__(self, **kwargs):
@@ -106,6 +131,7 @@ class mvapich2_gdr(rm, wget):
         # Trouble getting MRO with kwargs working correctly, so just call
         # the parent class constructors manually for now.
         #super(mvapich2_gdr, self).__init__(**kwargs)
+        ldconfig.__init__(self, **kwargs)
         rm.__init__(self, **kwargs)
         wget.__init__(self, **kwargs)
 
@@ -114,12 +140,14 @@ class mvapich2_gdr(rm, wget):
         self.__cuda_version = kwargs.get('cuda_version', '9.0')
         self.__gnu = kwargs.get('gnu', True)
         self.__gnu_version = '4.8.5'
-        self.__mofed_version = kwargs.get('mlnx_ofed_version', '3.4')
+        self.__install_path_template = '/opt/mvapich2/gdr/{0}/mcast/no-openacc/{1}/{2}/mpirun/{3}'
+        self.__mofed_version = kwargs.get('mlnx_ofed_version', '4.2')
         self.__ospackages = kwargs.get('ospackages', [])
         self.__package = kwargs.get('package', '')
+        self.__package_template = 'mvapich2-gdr-mcast.{0}.{1}.{2}-{3}-1.el7.x86_64.rpm'
         self.__pgi = kwargs.get('pgi', False)
         self.__pgi_version = '17.10'
-        self.version = kwargs.get('version', '2.3a')
+        self.version = kwargs.get('version', '2.3')
         self.__wd = '/var/tmp' # working directory
 
         # Output toolchain
@@ -127,7 +155,7 @@ class mvapich2_gdr(rm, wget):
                                    F90='mpif90', FC='mpifort')
 
         # Validate compiler choice
-        if self.__gnu and self.__pgi:
+        if self.__gnu and self.__pgi and not self.__package:
             logging.warning('Multiple compilers selected, using PGI')
             self.__gnu = False
         elif not self.__gnu and not self.__pgi:
@@ -137,9 +165,6 @@ class mvapich2_gdr(rm, wget):
         self.__commands = []              # Filled in by __setup()
         self.__environment_variables = {} # Filled in by __setup()
         self.__install_path = ''          # Filled in by __setup()
-
-        self.__installer = ''             # Filled in by __distro()
-        self.__package_template = ''      # Filled in by __distro()
 
         # Set the Linux distribution specific parameters
         self.__distro()
@@ -151,20 +176,9 @@ class mvapich2_gdr(rm, wget):
         """String representation of the building block"""
 
         instructions = []
-        if self.__package:
-            instructions.append(comment('MVAPICH2-GDR'))
-        else:
-            instructions.append(comment(
-                'MVAPICH2-GDR version {}'.format(self.version)))
-
+        instructions.append(comment(
+            'MVAPICH2-GDR version {}'.format(self.version)))
         instructions.append(packages(ospackages=self.__ospackages))
-
-        if self.__package:
-            # Use source from local build context
-            instructions.append(copy(src=self.__package,
-                                     dest=os.path.join(self.__wd,
-                                                       self.__package)))
-
         instructions.append(shell(commands=self.__commands))
         instructions.append(environment(
             variables=self.__environment_variables))
@@ -177,23 +191,32 @@ class mvapich2_gdr(rm, wget):
 
         if hpccm.config.g_linux_distro == linux_distro.UBUNTU:
             if not self.__ospackages:
-                self.__ospackages = ['openssh-client', 'wget']
-            self.__runtime_ospackages = ['openssh-client']
+                self.__ospackages = ['cpio', 'libnuma1', 'libpciaccess0',
+                                     'openssh-client', 'rpm2cpio', 'wget']
+                if self.__gnu:
+                    self.__ospackages.append('libgfortran3')
+            self.__runtime_ospackages = ['libnuma1', 'libpciaccess0',
+                                         'openssh-client']
+            if self.__gnu:
+                self.__runtime_ospackages.append('libgfortran3')
 
-            self.__installer = 'dpkg --install'
-
-            self.__package_template = 'mvapich2-gdr-mcast.{0}.{1}.{2}_{3}-1.el7.centos_amd64.deb'
+            self.__installer_template = 'cd / && rpm2cpio {} | cpio -id'
         elif hpccm.config.g_linux_distro == linux_distro.CENTOS:
             if not self.__ospackages:
-                self.__ospackages = ['openssh-clients', 'perl', 'wget']
-            self.__runtime_ospackages = ['openssh-clients']
+                self.__ospackages = ['libpciaccess', 'numactl-libs',
+                                     'openssh-clients', 'wget']
+                if self.__gnu:
+                    self.__ospackages.append('libgfortran')
+            self.__runtime_ospackages = ['libpciaccess', 'numactl-libs',
+                                         'openssh-clients']
+            if self.__gnu:
+                self.__runtime_ospackages.append('libgfortran')
 
             # The RPM has dependencies on some CUDA libraries that are
             # present, but not in the RPM database.  Use --nodeps as a
             # workaround.
-            self.__installer = 'rpm --install --nodeps'
+            self.__installer_template = 'rpm --install --nodeps {}'
 
-            self.__package_template = 'mvapich2-gdr-mcast.{0}.{1}.{2}-{3}-1.el7.centos.x86_64.rpm'
         else: # pragma: no cover
             raise RuntimeError('Unknown Linux distribution')
 
@@ -202,17 +225,16 @@ class mvapich2_gdr(rm, wget):
            i.e., fill in self.__commands and self.__environment_variables"""
 
         if self.__package:
-            # Install a package from the local build context
+            # Override individual settings and just use the specified package
             package = self.__package
 
             # Deduce version strings from package name
-            match = re.search(r'(?P<cuda>cuda\d+\.\d+)\.(?P<mofed>mofed\d+\.\d+)\.(?P<compiler>(gnu\d+\.\d+\.\d+)|(pgi\d+\.\d+))', package)
+            match = re.search(r'(?P<cuda>cuda\d+\.\d+)\.(?P<mofed>mofed\d+\.\d+)\.(?P<compiler>(gnu\d+\.\d+\.\d+)|(pgi\d+\.\d+))-(?P<version>\d+\.\d+)', package)
             cuda_string = match.groupdict()['cuda']
             mofed_string = match.groupdict()['mofed']
             compiler_string = match.groupdict()['compiler']
+            self.version = match.groupdict()['version']
         else:
-            # Download a package
-
             # Build the version strings based on the specified options
             if self.__gnu:
                 compiler_string = 'gnu{}'.format(self.__gnu_version)
@@ -229,15 +251,18 @@ class mvapich2_gdr(rm, wget):
             package = self.__package_template.format(
                 cuda_string, mofed_string, compiler_string, self.version)
 
-            # Download source from web
-            url = '{0}/{1}/{2}/{3}'.format(self.__baseurl, self.version,
-                                           mofed_string, package)
-            self.__commands.append(self.download_step(url=url,
-                                                      directory=self.__wd))
+        self.__install_path = self.__install_path_template.format(
+            self.version, cuda_string, mofed_string, compiler_string)
+
+        # Download source from web
+        url = '{0}/{1}/{2}/{3}'.format(self.__baseurl, self.version,
+                                       mofed_string, package)
+        self.__commands.append(self.download_step(url=url,
+                                                  directory=self.__wd))
 
         # Install the package
-        self.__commands.append('{0} {1}'.format(
-            self.__installer, os.path.join(self.__wd, package)))
+        self.__commands.append(
+            self.__installer_template.format(os.path.join(self.__wd, package)))
 
         # Workaround for bad path in the MPI compiler wrappers
         self.__commands.append('(test -f /usr/bin/bash || ln -s /bin/bash /usr/bin/bash)')
@@ -253,21 +278,20 @@ class mvapich2_gdr(rm, wget):
             items=[os.path.join(self.__wd, package)]))
 
         # Setup environment variables
-        self.__install_path = os.path.join('/opt', 'mvapich2', 'gdr',
-                                           self.version, 'mcast', 'no-openacc',
-                                           cuda_string, mofed_string,
-                                           'mpirun', compiler_string)
         self.__environment_variables = {
-            'LD_LIBRARY_PATH':
-            '{}:$LD_LIBRARY_PATH'.format(os.path.join(self.__install_path,
-                                                      'lib64')),
-            'MV2_USE_GPUDIRECT': 0,
-            'MV2_USE_GPUDIRECT_GDRCOPY': 0,
             'PATH': '{}:$PATH'.format(os.path.join(self.__install_path,
                                                    'bin')),
             # Workaround for using compiler wrappers in the build stage
             'PROFILE_POSTLIB': '"-L{} -lnvidia-ml"'.format(
                 '/usr/local/cuda/lib64/stubs')}
+
+        # Set library path
+        libpath = os.path.join(self.__install_path, 'lib64')
+        if self.ldconfig:
+            self.__commands.append(self.ldcache_step(directory=libpath))
+        else:
+            self.__environment_variables['LD_LIBRARY_PATH'] = '{}:$LD_LIBRARY_PATH'.format(libpath)
+
 
     def runtime(self, _from='0'):
         """Generate the set of instructions to install the runtime specific
@@ -286,9 +310,14 @@ class mvapich2_gdr(rm, wget):
         instructions.append(packages(ospackages=self.__runtime_ospackages))
         instructions.append(copy(src=self.__install_path,
                                  dest=self.__install_path, _from=_from))
-        # No need to workaround compiler wrapper issue for the runtime.
-        # Copy the dictionary so not to modify the original.
-        vars = dict(self.__environment_variables)
-        del vars['PROFILE_POSTLIB']
-        instructions.append(environment(variables=vars))
+        if self.ldconfig:
+            instructions.append(shell(
+                commands=[self.ldcache_step(
+                    directory=os.path.join(self.__install_path, 'lib64'))]))
+        if self.__environment_variables:
+            # No need to workaround compiler wrapper issue for the runtime.
+            # Copy the dictionary so not to modify the original.
+            vars = dict(self.__environment_variables)
+            del vars['PROFILE_POSTLIB']
+            instructions.append(environment(variables=vars))
         return '\n'.join(str(x) for x in instructions)
