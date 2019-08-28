8c8b6a52e9c3f08e5359123f4c434d2064709448
Scott McMillan <smcmillan@nvidia.com>
2019-08-28 16:58:32 -0500

    Bump component versions

--- recipes/julia/entrypoint.sh
+++ recipes/julia/entrypoint.sh
@@ -11,25 +11,25 @@ else
     # Singularity
     echo "Installing Julia CUDA packages (one-time) ..."
     julia -e 'using Pkg;
-              Pkg.add([PackageSpec(name="CUDAnative", rev="v2.1.3"),
-                       PackageSpec(name="CuArrays", rev="v1.0.2"),
-                       PackageSpec(name="CUDAdrv", rev="v3.0.1"),
-                       PackageSpec(name="GPUArrays", rev="v0.7.2")]);'
+              Pkg.add([PackageSpec(name="CUDAnative", rev="v2.3.0"),
+                       PackageSpec(name="CuArrays", rev="v1.2.1"),
+                       PackageSpec(name="CUDAdrv", rev="v3.1.0"),
+                       PackageSpec(name="GPUArrays", rev="v1.0.1")]);'
   else
     # Docker
     if [ $(id -u) -eq 0 ]; then
       # root
       echo "Re-building Julia CUDA driver package ..."
-      julia -e 'using Pkg; Pkg.add(PackageSpec(name="CUDAdrv", rev="v3.0.1")); Pkg.build("CuArrays")'
+      julia -e 'using Pkg; Pkg.add(PackageSpec(name="CUDAdrv", rev="v3.1.0"));'
     else
       # non-root
       export JULIA_DEPOT_PATH=/tmp/.julia-ngc
       echo "Installing Julia CUDA packages ..."
       julia -e 'using Pkg;
-                Pkg.add([PackageSpec(name="CUDAnative", rev="v2.1.3"),
-                         PackageSpec(name="CuArrays", rev="v1.0.2"),
-                         PackageSpec(name="CUDAdrv", rev="v3.0.1"),
-                         PackageSpec(name="GPUArrays", rev="v0.7.2")]);'
+                Pkg.add([PackageSpec(name="CUDAnative", rev="v2.3.0"),
+                         PackageSpec(name="CuArrays", rev="v1.2.1"),
+                         PackageSpec(name="CUDAdrv", rev="v3.1.0"),
+                         PackageSpec(name="GPUArrays", rev="v1.0.1")]);'
     fi
   fi
 fi
--- recipes/julia/julia.py
+++ recipes/julia/julia.py
@@ -6,9 +6,9 @@ Stage0 += baseimage(image='nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04')
 
 # Pin the version of each package for a reproducible container image build.
 Stage0 += julia(depot='~/.julia-ngc', ldconfig=True,
-                packages=['PackageSpec(name="CUDAnative", rev="v2.1.3")',
-                          'PackageSpec(name="CuArrays", rev="v1.0.2")',
-                          'PackageSpec(name="GPUArrays", rev="v0.7.2")'],
+                packages=['PackageSpec(name="CUDAnative", rev="v2.3.0")',
+                          'PackageSpec(name="CuArrays", rev="v1.2.1")',
+                          'PackageSpec(name="GPUArrays", rev="v1.0.1")'],
                 version='1.1.1')
 
 Stage0 += copy(src='examples', dest='/workspace/examples', _mkdir=True)
