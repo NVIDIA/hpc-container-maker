8b3286d5804437a4b59cfc7391dcbcc482c7bb76
Scott McMillan <scott.andrew.mcmillan@gmail.com>
2019-10-23 09:39:02 -0700

    Add Anaconda building block (#182)

---
+++ hpccm/building_blocks/conda.py
@@ -0,0 +1,207 @@
+# Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=invalid-name, too-few-public-methods
+# pylint: disable=too-many-instance-attributes
+
+"""conda building block"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from distutils.version import StrictVersion
+import logging # pylint: disable=unused-import
+import posixpath
+
+import hpccm.config
+import hpccm.templates.rm
+import hpccm.templates.wget
+
+from hpccm.building_blocks.base import bb_base
+from hpccm.building_blocks.packages import packages
+from hpccm.common import cpu_arch
+from hpccm.primitives.comment import comment
+from hpccm.primitives.copy import copy
+from hpccm.primitives.shell import shell
+
+class conda(bb_base, hpccm.templates.rm, hpccm.templates.wget):
+    """The `conda` building block installs Anaconda.
+
+    You must agree to the [Anaconda End User License Agreement](https://docs.anaconda.com/anaconda/eula/) to use this building block.
+
+    # Parameters
+
+    channels: List of additional Conda channels to enable.  The
+    default is an empty list.
+
+    eula: By setting this value to `True`, you agree to the [Anaconda End User License Agreement](https://docs.anaconda.com/anaconda/eula/).
+    The default value is `False`.
+
+    ospackages: List of OS packages to install prior to installing
+    Conda.  The default values are `ca-certificates` and `wget`.
+
+    packages: List of Conda packages to install.  The default is an
+    empty list.
+
+    prefix: The top level install location.  The default value is
+    `/usr/local/anaconda`.
+
+    python2: Boolean flag to specify that the Python 2 version of
+    Anaconda should be installed.  The default is False.
+
+    version: The version of Anaconda to download.  The default value
+    is `4.7.12`.
+
+    # Examples
+
+    ```python
+    conda(packages=['numpy'])
+    ```
+
+    ```python
+    conda(channels=['conda-forge', 'nvidia'], prefix='/opt/conda')
+    ```
+
+    """
+
+    def __init__(self, **kwargs):
+        """Initialize building block"""
+
+        super(conda, self).__init__(**kwargs)
+
+        self.__arch_pkg = '' # Filled in by __cpu_arch()
+        self.__baseurl = kwargs.get('baseurl',
+                                    'http://repo.anaconda.com/miniconda')
+        self.__channels = kwargs.get('channels', [])
+
+        # By setting this value to True, you agree to the
+        # corresponding Anaconda End User License Agreement
+        # https://docs.anaconda.com/anaconda/eula/
+        self.__eula = kwargs.get('eula', False)
+        self.__ospackages = kwargs.get('ospackages',
+                                       ['ca-certificates', 'wget'])
+        self.__packages = kwargs.get('packages', [])
+        self.__prefix = kwargs.get('prefix', '/usr/local/anaconda')
+        self.__python2 = kwargs.get('python2', False)
+        self.__python_version = '2' if self.__python2 else '3'
+        self.__version = kwargs.get('version', '4.7.12')
+
+        self.__commands = [] # Filled in by __setup()
+        self.__wd = '/var/tmp' # working directory
+
+        if not self.__eula:
+            logging.warning('Anaconda EULA was not accepted.  To accept, see the documentation for this building block')
+
+        # Set the CPU architecture specific parameters
+        self.__cpu_arch()
+
+        # Construct the series of steps to execute
+        self.__setup()
+
+        # Fill in container instructions
+        self.__instructions()
+
+    def __instructions(self):
+        """Fill in container instructions"""
+
+        self += comment('Anaconda')
+        self += packages(ospackages=self.__ospackages)
+        self += shell(commands=self.__commands)
+
+    def __cpu_arch(self):
+        """Based on the CPU architecture, set values accordingly.  A user
+        specified value overrides any defaults."""
+
+        if hpccm.config.g_cpu_arch == cpu_arch.PPC64LE:
+            self.__arch_pkg = 'ppc64le'
+        elif hpccm.config.g_cpu_arch == cpu_arch.X86_64:
+            self.__arch_pkg = 'x86_64'
+        else: # pragma: no cover
+            raise RuntimeError('Unknown CPU architecture')
+
+    def __setup(self):
+        """Construct the series of shell commands, i.e., fill in
+           self.__commands"""
+
+        miniconda = 'Miniconda{0}-{1}-Linux-{2}.sh'.format(
+            self.__python_version, self.__version, self.__arch_pkg)
+        url = '{0}/{1}'.format(self.__baseurl, miniconda)
+
+        # Download source from web
+        self.__commands.append(self.download_step(url=url,
+                                                  directory=self.__wd))
+
+        # Install
+        install_args = ['-p {}'.format(self.__prefix)]
+        if self.__eula:
+            install_args.append('-b')
+        self.__commands.append('bash {0} {1}'.format(
+            posixpath.join(self.__wd, miniconda),
+            ' '.join(sorted(install_args))))
+
+        # Initialize conda
+        self.__commands.append('{0} init'.format(
+            posixpath.join(self.__prefix, 'bin', 'conda')))
+        self.__commands.append('ln -s {} /etc/profile.d/conda.sh'.format(
+            posixpath.join(self.__prefix, 'etc', 'profile.d', 'conda.sh')))
+
+        # Activate
+        if self.__channels or self.__packages:
+            self.__commands.append('. {}'.format(
+                posixpath.join(self.__prefix, 'etc', 'profile.d', 'conda.sh')))
+            self.__commands.append('conda activate base')
+
+        # Enable channels
+        if self.__channels:
+            self.__commands.append('conda config {}'.format(
+                ' '.join(['--add channels {}'.format(x)
+                          for x in sorted(self.__channels)])))
+
+        # Install conda packages
+        if self.__packages:
+            self.__commands.append('conda install -y {}'.format(
+                ' '.join(sorted(self.__packages))))
+
+        # Cleanup conda install
+        self.__commands.append('{0} clean -afy'.format(
+            posixpath.join(self.__prefix, 'bin', 'conda')))
+
+        # Cleanup miniconda download file
+        self.__commands.append(self.cleanup_step(
+            items=[posixpath.join(self.__wd, miniconda)]))
+
+    def runtime(self, _from='0'):
+        """Generate the set of instructions to install the runtime specific
+        components from a build in a previous stage.
+
+        # Examples
+
+        ```python
+        c = conda(...)
+        Stage0 += c
+        Stage1 += c.runtime()
+        ```
+        """
+        instructions = []
+        instructions.append(comment('Anaconda'))
+        instructions.append(copy(_from=_from, src=self.__prefix,
+                                 dest=self.__prefix))
+        instructions.append(shell(commands=[
+            '{0} init'.format(
+                posixpath.join(self.__prefix, 'bin', 'conda')),
+            'ln -s {0} /etc/profile.d/conda.sh'.format(
+                posixpath.join(self.__prefix, 'etc', 'profile.d',
+                               'conda.sh'))]))
+        return '\n'.join(str(x) for x in instructions)
---
+++ test/test_conda.py
@@ -0,0 +1,153 @@
+# Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=invalid-name, too-few-public-methods, bad-continuation
+
+"""Test cases for the conda module"""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import logging # pylint: disable=unused-import
+import unittest
+
+from helpers import centos, docker, ppc64le, ubuntu, x86_64
+
+from hpccm.building_blocks.conda import conda
+
+class Test_conda(unittest.TestCase):
+    def setUp(self):
+        """Disable logging output messages"""
+        logging.disable(logging.ERROR)
+
+    @x86_64
+    @ubuntu
+    @docker
+    def test_defaults_ubuntu(self):
+        """Default conda building block"""
+        c = conda(eula=True, packages=['numpy'])
+        self.assertEqual(str(c),
+r'''# Anaconda
+RUN apt-get update -y && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        ca-certificates \
+        wget && \
+    rm -rf /var/lib/apt/lists/*
+RUN mkdir -p /var/tmp && wget -q -nc --no-check-certificate -P /var/tmp http://repo.anaconda.com/miniconda/Miniconda3-4.7.12-Linux-x86_64.sh && \
+    bash /var/tmp/Miniconda3-4.7.12-Linux-x86_64.sh -b -p /usr/local/anaconda && \
+    /usr/local/anaconda/bin/conda init && \
+    ln -s /usr/local/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    . /usr/local/anaconda/etc/profile.d/conda.sh && \
+    conda activate base && \
+    conda install -y numpy && \
+    /usr/local/anaconda/bin/conda clean -afy && \
+    rm -rf /var/tmp/Miniconda3-4.7.12-Linux-x86_64.sh''')
+
+    @x86_64
+    @centos
+    @docker
+    def test_defaults_centos(self):
+        """Default conda building block"""
+        c = conda(eula=True, packages=['numpy'])
+        self.assertEqual(str(c),
+r'''# Anaconda
+RUN yum install -y \
+        ca-certificates \
+        wget && \
+    rm -rf /var/cache/yum/*
+RUN mkdir -p /var/tmp && wget -q -nc --no-check-certificate -P /var/tmp http://repo.anaconda.com/miniconda/Miniconda3-4.7.12-Linux-x86_64.sh && \
+    bash /var/tmp/Miniconda3-4.7.12-Linux-x86_64.sh -b -p /usr/local/anaconda && \
+    /usr/local/anaconda/bin/conda init && \
+    ln -s /usr/local/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    . /usr/local/anaconda/etc/profile.d/conda.sh && \
+    conda activate base && \
+    conda install -y numpy && \
+    /usr/local/anaconda/bin/conda clean -afy && \
+    rm -rf /var/tmp/Miniconda3-4.7.12-Linux-x86_64.sh''')
+
+    @ppc64le
+    @ubuntu
+    @docker
+    def test_ppc64le(self):
+        """ppc64le"""
+        c = conda(eula=True)
+        self.assertEqual(str(c),
+r'''# Anaconda
+RUN apt-get update -y && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        ca-certificates \
+        wget && \
+    rm -rf /var/lib/apt/lists/*
+RUN mkdir -p /var/tmp && wget -q -nc --no-check-certificate -P /var/tmp http://repo.anaconda.com/miniconda/Miniconda3-4.7.12-Linux-ppc64le.sh && \
+    bash /var/tmp/Miniconda3-4.7.12-Linux-ppc64le.sh -b -p /usr/local/anaconda && \
+    /usr/local/anaconda/bin/conda init && \
+    ln -s /usr/local/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    /usr/local/anaconda/bin/conda clean -afy && \
+    rm -rf /var/tmp/Miniconda3-4.7.12-Linux-ppc64le.sh''')
+
+    @x86_64
+    @ubuntu
+    @docker
+    def test_channels(self):
+        """channels"""
+        c = conda(channels=['conda-forge', 'nvidia'], eula=True)
+        self.assertEqual(str(c),
+r'''# Anaconda
+RUN apt-get update -y && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        ca-certificates \
+        wget && \
+    rm -rf /var/lib/apt/lists/*
+RUN mkdir -p /var/tmp && wget -q -nc --no-check-certificate -P /var/tmp http://repo.anaconda.com/miniconda/Miniconda3-4.7.12-Linux-x86_64.sh && \
+    bash /var/tmp/Miniconda3-4.7.12-Linux-x86_64.sh -b -p /usr/local/anaconda && \
+    /usr/local/anaconda/bin/conda init && \
+    ln -s /usr/local/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    . /usr/local/anaconda/etc/profile.d/conda.sh && \
+    conda activate base && \
+    conda config --add channels conda-forge --add channels nvidia && \
+    /usr/local/anaconda/bin/conda clean -afy && \
+    rm -rf /var/tmp/Miniconda3-4.7.12-Linux-x86_64.sh''')
+
+    @x86_64
+    @ubuntu
+    @docker
+    def test_python2(self):
+        """python 2"""
+        c = conda(eula=True, python2=True)
+        self.assertEqual(str(c),
+r'''# Anaconda
+RUN apt-get update -y && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        ca-certificates \
+        wget && \
+    rm -rf /var/lib/apt/lists/*
+RUN mkdir -p /var/tmp && wget -q -nc --no-check-certificate -P /var/tmp http://repo.anaconda.com/miniconda/Miniconda2-4.7.12-Linux-x86_64.sh && \
+    bash /var/tmp/Miniconda2-4.7.12-Linux-x86_64.sh -b -p /usr/local/anaconda && \
+    /usr/local/anaconda/bin/conda init && \
+    ln -s /usr/local/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    /usr/local/anaconda/bin/conda clean -afy && \
+    rm -rf /var/tmp/Miniconda2-4.7.12-Linux-x86_64.sh''')
+
+    @x86_64
+    @ubuntu
+    @docker
+    def test_runtime(self):
+        """runtime"""
+        c = conda(eula=True)
+        r = c.runtime()
+        self.assertEqual(r,
+r'''# Anaconda
+COPY --from=0 /usr/local/anaconda /usr/local/anaconda
+RUN /usr/local/anaconda/bin/conda init && \
+    ln -s /usr/local/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh''')
