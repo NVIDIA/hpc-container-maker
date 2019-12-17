55e100d01053de60c1ff608166370c11d0af8760
Lars Bilke <lars.bilke@ufz.de>
2019-12-17 16:16:34 +0100

    [pmix] Added libevent-pthreads as another runtime dependency on Ubuntu.
    There are maybe even more:
    
    https://packages.ubuntu.com/bionic/libevent-dev
    
    but the libevent-pthreads was the only missing dependency for a
    OpenMPI application.

--- test/test_pmix.py
+++ test/test_pmix.py
@@ -95,7 +95,8 @@ ENV CPATH=/usr/local/pmix/include:$CPATH \
 r'''# PMIX
 RUN apt-get update -y && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-        libevent-2.* && \
+        libevent-2.* \
+        libevent-pthreads-2.* && \
     rm -rf /var/lib/apt/lists/*
 COPY --from=0 /usr/local/pmix /usr/local/pmix
 ENV CPATH=/usr/local/pmix/include:$CPATH \
