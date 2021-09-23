a27d65833fca3910c130d5efce5d6a9c87602663
Scott McMillan <smcmillan@nvidia.com>
2021-09-23 15:51:26 -0500

    Bump NVHPC to 21.9

--- hpccm/building_blocks/nvhpc.py
+++ hpccm/building_blocks/nvhpc.py
@@ -102,7 +102,7 @@ class nvhpc(bb_base, hpccm.templates.downloader, hpccm.templates.envvars,
 
     version: The version of the HPC SDK to use.  Note when `package`
     is set the version is determined automatically from the package
-    file name.  The default value is `21.7`.
+    file name.  The default value is `21.9`.
 
     # Examples
 
@@ -153,7 +153,7 @@ class nvhpc(bb_base, hpccm.templates.downloader, hpccm.templates.envvars,
         self.__redist = kwargs.get('redist', [])
         self.__stdpar_cudacc = kwargs.get('stdpar_cudacc', None)
         self.__url = kwargs.get('url', None)
-        self.__version = kwargs.get('version', '21.7')
+        self.__version = kwargs.get('version', '21.9')
         self.__wd = kwargs.get('wd', hpccm.config.g_wd) # working directory
         self.__year = '' # Filled in by __version()
 
@@ -290,8 +290,11 @@ class nvhpc(bb_base, hpccm.templates.downloader, hpccm.templates.envvars,
                 posixpath.join(self.__basepath, 'comm_libs', 'mpi', 'bin'))
         elif self.__hpcx:
             # Set environment for HPC-X
-            hpcx_version = '2.8.1'
-            if StrictVersion(self.__version) < StrictVersion('21.5'):
+            if StrictVersion(self.__version) >= StrictVersion('21.9'):
+                hpcx_version = '2.9.0'
+            elif StrictVersion(self.__version) >= StrictVersion('21.7'):
+                hpcx_version = '2.8.1'
+            elif StrictVersion(self.__version) < StrictVersion('21.5'):
               hpcx_version = '2.7.4'
             hpcx_dir = posixpath.join(self.__basepath, 'comm_libs', 'hpcx',
                                       'hpcx-{}'.format(hpcx_version))
