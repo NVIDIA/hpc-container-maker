9f04d168cb1c1e16c97a108a5a6a01fff8e95c71
Scott McMillan <smcmillan@nvidia.com>
2023-10-11 09:09:47 -0500

    Update LLVM trunk version to 18 (#472)

--- test/test_llvm.py
+++ test/test_llvm.py
@@ -361,15 +361,15 @@ RUN wget -qO - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add - && \
     echo "deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic main" >> /etc/apt/sources.list.d/hpccm.list && \
     apt-get update -y && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-        clang-17 \
-        clang-format-17 \
-        clang-tidy-17 \
-        libomp-17-dev && \
+        clang-18 \
+        clang-format-18 \
+        clang-tidy-18 \
+        libomp-18-dev && \
     rm -rf /var/lib/apt/lists/*
-RUN update-alternatives --install /usr/bin/clang clang $(which clang-17) 30 && \
-    update-alternatives --install /usr/bin/clang++ clang++ $(which clang++-17) 30 && \
-    update-alternatives --install /usr/bin/clang-format clang-format $(which clang-format-17) 30 && \
-    update-alternatives --install /usr/bin/clang-tidy clang-tidy $(which clang-tidy-17) 30''')
+RUN update-alternatives --install /usr/bin/clang clang $(which clang-18) 30 && \
+    update-alternatives --install /usr/bin/clang++ clang++ $(which clang++-18) 30 && \
+    update-alternatives --install /usr/bin/clang-format clang-format $(which clang-format-18) 30 && \
+    update-alternatives --install /usr/bin/clang-tidy clang-tidy $(which clang-tidy-18) 30''')
 
     @x86_64
     @ubuntu20
@@ -391,15 +391,15 @@ RUN wget -qO - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add - && \
     echo "deb-src http://apt.llvm.org/focal/ llvm-toolchain-focal main" >> /etc/apt/sources.list.d/hpccm.list && \
     apt-get update -y && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-        clang-17 \
-        clang-format-17 \
-        clang-tidy-17 \
-        libomp-17-dev && \
+        clang-18 \
+        clang-format-18 \
+        clang-tidy-18 \
+        libomp-18-dev && \
     rm -rf /var/lib/apt/lists/*
-RUN update-alternatives --install /usr/bin/clang clang $(which clang-17) 30 && \
-    update-alternatives --install /usr/bin/clang++ clang++ $(which clang++-17) 30 && \
-    update-alternatives --install /usr/bin/clang-format clang-format $(which clang-format-17) 30 && \
-    update-alternatives --install /usr/bin/clang-tidy clang-tidy $(which clang-tidy-17) 30''')
+RUN update-alternatives --install /usr/bin/clang clang $(which clang-18) 30 && \
+    update-alternatives --install /usr/bin/clang++ clang++ $(which clang++-18) 30 && \
+    update-alternatives --install /usr/bin/clang-format clang-format $(which clang-format-18) 30 && \
+    update-alternatives --install /usr/bin/clang-tidy clang-tidy $(which clang-tidy-18) 30''')
 
     @aarch64
     @ubuntu
